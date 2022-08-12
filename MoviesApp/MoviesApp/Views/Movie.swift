9423b3f8d792144b2e56c7c47837877e3bad0733
Tudor Ghilvacs <tudor.ghilvacs@softvision.com>
2022-08-12 16:39:09 +0300

    Movies main page

---
+++ MoviesApp/MoviesApp/Views/Movie.swift
@@ -0,0 +1,14 @@
+//
+//  Movie.swift
+//  MoviesApp
+//
+//  Created by Tudor Ghilvacs on 12.08.2022.
+//
+
+import Foundation
+
+struct Movie {
+    var name: String?
+    var rating: Double?
+    var year: Int?
+}
